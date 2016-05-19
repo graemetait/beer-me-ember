a60bd751c31da1a504d7d69d8d7371254c057397
Graeme Tait <burriko@gmail.com>
2016-05-19 14:55:44 +0100

    Add slug

--- app/components/beer-list-item/component.js
+++ app/components/beer-list-item/component.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
+import slug from 'npm:slug';
 
 export default Ember.Component.extend({
   classNames: ['beer'],
   imageURL: Ember.computed('beer', function() {
     const beer = this.get('beer');
     return beer.labels && beer.labels.medium || 'null.jpg';
+  }),
+  slug: Ember.computed('beer.name', function() {
+    return slug(this.get('beer.name'));
   })
 });
