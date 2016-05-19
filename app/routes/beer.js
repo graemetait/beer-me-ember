91ee4f6fa3212ea09a552e692311cbd309514ec2
Graeme Tait <burriko@gmail.com>
2016-05-19 16:31:35 +0100

    Show beer details

--- app/routes/beer.js
+++ app/routes/beer.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    const { beerId } = params;
+    return $.getJSON(`http://api.react.beer/v2/beer/${beerId}`).then(beer => beer.data);
+  }
 });
