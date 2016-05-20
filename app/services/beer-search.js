9701db2ec2402f4f7e606bea7897f0812460ea0c
Graeme Tait <burriko@gmail.com>
2016-05-20 10:33:39 +0100

    Cache results in local storage

--- app/services/beer-search.js
+++ app/services/beer-search.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   search(searchTerm = 'hops') {
+
+    const localStorageBeers = localStorage.getItem(`search-${searchTerm}`);
+    if (localStorageBeers) {
+      return JSON.parse(localStorageBeers);
+    }
+
     console.log(`fetching beers with ${searchTerm}`);
     return $.getJSON(`http://api.react.beer/v2/search?q=${searchTerm}&type=beer`)
-      .then(beers => beers.data.filter(beer => !!beer.labels));
+      .then(beers => {
+        const filteredBeers = beers.data.filter(beer => !!beer.labels);
+        localStorage.setItem(`search-${searchTerm}`, JSON.stringify(filteredBeers));
+        return filteredBeers;
+      });
   }
 });
